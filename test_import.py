9199a68a9cc578b88ff425b1301cc7ae491f1b98
Rakshita <rakshita@11102006@gmail.com>
2026-08-30 23:22:29 +0530

    Remove temporary test scripts

--- test_import.py
+++
@@ -1,1 +0,0 @@
-try:\n    import google.generativeai as genai\n    print(" genai imported\)\nexcept Exception as e:\n print(\Import error\, e)
--- test_import2.py
+++
@@ -1,5 +0,0 @@
-try:
-    import google.generativeai as genai
-    print('genai imported')
-except Exception as e:
-    print('Import error', e)
